8eb9174d331b18b88671a3f9ed504dd1c35085b6
Robbie Wagner <robertmwagneriii@gmail.com>
2023-10-19 16:57:39 -0500

    fix bugs, add new scripts

---
+++ RobbieWagnerGames/Utilities/SiblingRuleTile.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace RobbieWagnerGames
+{
+    [CreateAssetMenu]
+    public class SiblingRuleTile : RuleTile {
+        public enum SiblingGroup
+        {
+            Blank,
+        }
+
+        public SiblingGroup siblingGroup;
+
+        public override bool RuleMatch(int neighbor, TileBase other)
+        {
+            if (other is RuleOverrideTile)
+                other = (other as RuleOverrideTile).m_InstanceTile;
+
+            switch (neighbor)
+            {
+                case TilingRule.Neighbor.This:
+                    {
+                        return other is SiblingRuleTile
+                            && (other as SiblingRuleTile).siblingGroup == this.siblingGroup;
+                    }
+                case TilingRule.Neighbor.NotThis:
+                    {
+                        return !(other is SiblingRuleTile
+                            && (other as SiblingRuleTile).siblingGroup == this.siblingGroup);
+                    }
+            }
+
+            return base.RuleMatch(neighbor, other);
+        }
+    }
+}
