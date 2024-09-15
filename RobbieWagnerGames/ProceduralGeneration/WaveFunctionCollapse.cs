bd3535eb5fec8c128289354bd5b6698a66337d4a
Robbie_Wagner <robbie.zoborowi@gmail.com>
2024-09-15 09:44:04 -0500

    Update scripts, extensions

--- RobbieWagnerGames/ProceduralGeneration/WaveFunctionCollapse.cs
+++ RobbieWagnerGames/ProceduralGeneration/WaveFunctionCollapse.cs
@@ -5,7 +5,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
-using static UnityEditor.Rendering.CameraUI;
 
 namespace RobbieWagnerGames.ProcGen
 {
