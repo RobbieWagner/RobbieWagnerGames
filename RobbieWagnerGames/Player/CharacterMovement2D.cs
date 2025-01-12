8db6e4d7a6f353708fba667da97cb0d7c478311a
Robbie_Wagner <robbie.zoborowi@gmail.com>
2025-01-12 11:40:49 -0600

    Update setup

--- RobbieWagnerGames/Player/CharacterMovement2D.cs
+++ RobbieWagnerGames/Player/CharacterMovement2D.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections;
+using RobbieWagnerGames;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using RobbieWagnerGames;
-using System.Security.Cryptography;
 
 public class CharacterMovement2D : MonoBehaviour
 {
@@ -12,67 +9,86 @@ public class CharacterMovement2D : MonoBehaviour
     public bool canMove = true;
     [HideInInspector] public bool moving = false;
     private UnitAnimationState currentAnimationState;
-    private PlayerInputActions inputActions;
     private Vector2 movementVector = Vector2.zero;
-    [SerializeField] private float currentWalkSpeed;
+    [SerializeField] private float currentWalkSpeed = 3;
+    public Rigidbody2D rb;
+    public LayerMask collisionLayers;
 
     [SerializeField] private AudioSource footstepAudioSource;
 
     private Vector3 lastFramePos = Vector3.zero;
 
-    [SerializeField][Range(-1, 0)] private float wallPushback = -.08f;
+    //[SerializeField][Range(-1, 0)] private float wallPushback = -.08f;
     private HashSet<Collider2D> colliders;
+
+    public static CharacterMovement2D Instance { get; private set; }
 
     private void Awake()
     {
-        inputActions = new PlayerInputActions();
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
         colliders = new HashSet<Collider2D>();
+
+        InputManager.Instance.gameControls.PLAYER.Move.performed += OnMove;
+        InputManager.Instance.gameControls.PLAYER.Move.canceled += StopPlayer;
     }
 
-    private void LateUpdate() 
+    private void FixedUpdate() 
     {
-
-        if(canMove) 
+        if (canMove && movementVector.y != 0)
         {
-            transform.Translate(movementVector * currentWalkSpeed * Time.deltaTime);
-            foreach(Collider2D collider in colliders)
-                transform.position = Vector2.MoveTowards(transform.position, collider.transform.position, wallPushback);
+            Vector2 normalizedMovement = movementVector.normalized;
+            Vector2 desiredVelocity = normalizedMovement * currentWalkSpeed;
+
+            Debug.Log(normalizedMovement);
+
+            Vector2 verticalMove = new Vector2(0, desiredVelocity.y);
+            RaycastHit2D hitVertical = Physics2D.Raycast(rb.position + verticalMove.normalized * .301f, verticalMove, Mathf.Abs(verticalMove.normalized.y * .001f), collisionLayers);
+
+            float finalY = hitVertical.collider == null ? verticalMove.y : 0;
+
+            rb.velocity = new Vector2(0, finalY);
         }
-
-        lastFramePos = transform.position;
-
-        if(moving && footstepAudioSource != null && !footstepAudioSource.isPlaying) 
+        else
         {
-            PlayMovementSounds();
+            // No movement input
+            rb.velocity = Vector2.zero;
         }
     }
 
-    private void OnMove(InputValue inputValue)
+    private void OnMove(InputAction.CallbackContext context)
     {
         if(canMove)
         {
-            Vector2 input = inputValue.Get<Vector2>();
+            Vector2 input = context.ReadValue<Vector2>();
 
             if(movementVector.x != input.x && input.x != 0f)
             {
-                if(input.x > 0) unitAnimator.ChangeAnimationState(UnitAnimationState.WalkRight);
-                else unitAnimator.ChangeAnimationState(UnitAnimationState.WalkLeft);
+                //if(input.x > 0) unitAnimator.ChangeAnimationState(UnitAnimationState.WalkRight);
+                //else unitAnimator.ChangeAnimationState(UnitAnimationState.WalkLeft);
                 moving = true;
             }
             else if(input.x == 0 && movementVector.y != input.y && input.y != 0f)
             {
-                if(input.y > 0) unitAnimator.ChangeAnimationState(UnitAnimationState.WalkForward);
-                else unitAnimator.ChangeAnimationState(UnitAnimationState.WalkBack);
+                //if(input.y > 0) unitAnimator.ChangeAnimationState(UnitAnimationState.WalkForward);
+                //else unitAnimator.ChangeAnimationState(UnitAnimationState.WalkBack);
                 moving = true;
             }
             else if(input.x == 0 && input.y == 0)
             {
-                if(movementVector.x > 0)unitAnimator.ChangeAnimationState(UnitAnimationState.IdleRight);
-                else if(movementVector.x < 0)unitAnimator.ChangeAnimationState(UnitAnimationState.IdleLeft);
-                else if(movementVector.y > 0) unitAnimator.ChangeAnimationState(UnitAnimationState.IdleForward);
-                else unitAnimator.ChangeAnimationState(UnitAnimationState.Idle);
+                //if(movementVector.x > 0)unitAnimator.ChangeAnimationState(UnitAnimationState.IdleRight);
+                //else if(movementVector.x < 0)unitAnimator.ChangeAnimationState(UnitAnimationState.IdleLeft);
+                //else if(movementVector.y > 0) unitAnimator.ChangeAnimationState(UnitAnimationState.IdleForward);
+                //else unitAnimator.ChangeAnimationState(UnitAnimationState.Idle);
                 moving = false;
-                if(footstepAudioSource != null) StopMovementSounds();
+                //if(footstepAudioSource != null) StopMovementSounds();
             }
 
             movementVector = input;
@@ -84,16 +100,21 @@ public class CharacterMovement2D : MonoBehaviour
         StopPlayer();
     }
 
+    public void StopPlayer(InputAction.CallbackContext context)
+    {
+        StopPlayer();
+    }
+
     public void StopPlayer()
     {
-        if(movementVector.x > 0) unitAnimator.ChangeAnimationState(UnitAnimationState.IdleRight);
-        else if(movementVector.x < 0) unitAnimator.ChangeAnimationState(UnitAnimationState.IdleLeft);
-        else if(movementVector.y > 0) unitAnimator.ChangeAnimationState(UnitAnimationState.IdleForward);
-        else if(movementVector != Vector2.zero) unitAnimator.ChangeAnimationState(UnitAnimationState.Idle);
+        //if(movementVector.x > 0) unitAnimator.ChangeAnimationState(UnitAnimationState.IdleRight);
+        //else if(movementVector.x < 0) unitAnimator.ChangeAnimationState(UnitAnimationState.IdleLeft);
+        //else if(movementVector.y > 0) unitAnimator.ChangeAnimationState(UnitAnimationState.IdleForward);
+        //else if(movementVector != Vector2.zero) unitAnimator.ChangeAnimationState(UnitAnimationState.Idle);
 
         movementVector = Vector3.zero;
         moving = false;
-        if(footstepAudioSource != null) StopMovementSounds();
+        //if(footstepAudioSource != null) StopMovementSounds();
     }
 
     public void CeasePlayerMovement()
@@ -102,19 +123,18 @@ public class CharacterMovement2D : MonoBehaviour
         StopPlayer();
     }
 
-    public void PlayMovementSounds()
-    {
-        footstepAudioSource.Play();
-    }
+    //public void PlayMovementSounds()
+    //{
+    //    footstepAudioSource.Play();
+    //}
 
-    public void StopMovementSounds()
-    {
-        footstepAudioSource.Stop();
-    }
+    //public void StopMovementSounds()
+    //{
+    //    footstepAudioSource.Stop();
+    //}
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("new collision");
         colliders.Add(other.collider);
     }
 
---
+++ RobbieWagnerGames/Utilities/Singleton/FusionNetworkBehaviourSingleton.cs
@@ -0,0 +1,44 @@
+﻿using Fusion;
+using System;
+
+namespace Assets.Scripts.RobbieWagnerGames.Utilities.Singleton
+{
+    public abstract class FusionNetworkBehaviourSingleton<T> : NetworkBehaviour where T : FusionNetworkBehaviourSingleton<T>
+    {
+        public static event Action<FusionNetworkBehaviourSingleton<T>> OnInstanceSet;
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                return instance;
+            }
+            protected set
+            {
+                if (instance == value)
+                    return;
+                instance = value;
+                OnInstanceSet?.Invoke(instance);
+            }
+        }
+
+        public static bool hasInstance => instance != null;
+
+        protected virtual void Awake()
+        {
+            if (instance != null)
+                Destroy(gameObject);
+            else
+            {
+                instance = (T)this;
+                OnInstanceSet?.Invoke(this);
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (instance == this)
+                instance = null;
+        }
+    }
+}
